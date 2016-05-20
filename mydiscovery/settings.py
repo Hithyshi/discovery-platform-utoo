30db001a3960ad6922e55529b69da1bdcfc899a4
Hithyshi Krishnamurthy <hithyshikrishnamurthy@Hithyshis-MacBook-Pro.local>
2016-05-20 11:23:15 +0530

    adding popup2

--- mydiscovery/settings.py
+++ mydiscovery/settings.py
@@ -122,13 +122,22 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(PROJECT_ROOT, 'static'),
+#]
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
